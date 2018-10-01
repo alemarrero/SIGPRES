fbda0a7499b6b263702fe18155bdc7e6492e4068
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-01 11:20:27 -0400

    se agregan algunos comentarios

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 const dotenv = require('dotenv').config();
 var session = require('express-session');
 
+// Routers
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var cors = require('cors');
 
+// Se inicializa la app de express
 var app = express();
 
+
+// Middlewares
 app.use(
     session({
     secret: 'keyboard cat',
@@ -27,6 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas
 app.use('/', indexRouter);
 app.use('/api/auth', authRouter);
 
