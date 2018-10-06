c0b1e4531e55174874fc113f85f9072afc7ed15d
Alessandra Marrero <alemarrero16@gmail.com>
2018-10-06 17:18:50 -0400

    Se arreglan los íconos de los módulos del menú

--- client/src/Componentes/Menu/Menu.js
+++ client/src/Componentes/Menu/Menu.js
@@ -5,49 +5,36 @@ import management from '../../assets/img/management.png';
 import budget from '../../assets/img/budget.png';
 import solution from '../../assets/img/solution.png';
 
-
 export default class Menu extends Component {
   render() {
     return (
         <Row className="fila-opciones-menu-principal">
             {/* Gestión de usuarios */}
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
-<<<<<<< HEAD
-            <Card onClick={() => this.props.history.push('/usuarios')} body outline color="success">
-=======
-            <Card onClick={() => this.props.history.push(this.props.match.path + 'usuarios')}>
->>>>>>> 162b344e480525939598937d9000dd8bb38dc0b5
+            <Card onClick={() => this.props.history.push(this.props.match.path + 'usuarios')} body outline color="success">
                 <CardBody>
-                <img src={management}/>    
-                <h2>Gestión de usuarios</h2>
+                <img src={management} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Gestión de usuarios</h2>
                 </CardBody>
             </Card>
             </Col>    
             
             {/* Gestión presupuestal  */}
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
-<<<<<<< HEAD
-            <Card onClick={() => this.props.history.push('/presupuesto')} body outline color="success">
-=======
-            <Card onClick={() => this.props.history.push(this.props.match.path + 'presupuesto')}>
->>>>>>> 162b344e480525939598937d9000dd8bb38dc0b5
+            <Card onClick={() => this.props.history.push(this.props.match.path + 'presupuesto')} body outline color="success">
                 <CardBody>
-                <img src={budget}/>    
-                <h2>Gestión presupuestal</h2>
+                <img src={budget} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Gestión presupuestal</h2>
                 </CardBody>
             </Card>
             </Col>
 
             {/* Gestión de objetivos específicos */}
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
-<<<<<<< HEAD
-            <Card onClick={() => this.props.history.push('/objetivos-especificos')} body outline color="success">
-=======
-            <Card onClick={() => this.props.history.push(this.props.match.path + 'objetivos-especificos')}>
->>>>>>> 162b344e480525939598937d9000dd8bb38dc0b5
+            <Card onClick={() => this.props.history.push(this.props.match.path + 'objetivos-especificos')} body outline color="success">
                 <CardBody>
-                <img src={solution}/>    
-                <h2>Gestión de planeación</h2>
+                <img src={solution} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Gestión de planeación</h2>
                 </CardBody>
             </Card>
             </Col>
