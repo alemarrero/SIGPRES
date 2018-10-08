7097fc79908cdf7b66cd21e706787516b51a4954
Alessandra Marrero <alemarrero16@gmail.com>
2018-10-07 20:56:37 -0400

    se borra gestión usuarios y se agrega administración al menú principal

--- client/src/Componentes/Menu/Menu.js
+++ client/src/Componentes/Menu/Menu.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {Row, Col, Card, CardBody} from 'reactstrap';
 import './Menu.css';
-import management from '../../assets/img/management.png';
-import budget from '../../assets/img/budget.png';
-import solution from '../../assets/img/solution.png';
+import administracion from '../../assets/img/work-team.png';
+import presupuesto from '../../assets/img/budget.png';
+import planeacion from '../../assets/img/solution.png';
 
 export default class Menu extends Component {
   render() {
@@ -11,10 +11,10 @@ export default class Menu extends Component {
         <Row className="fila-opciones-menu-principal">
             {/* Gestión de usuarios */}
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
-            <Card onClick={() => this.props.history.push(this.props.match.path + 'usuarios')} body outline color="success">
+            <Card onClick={() => this.props.history.push(this.props.match.path + 'administracion/')} body outline color="success">
                 <CardBody>
-                <img src={management} className="iconos-menu"/>    
-                <h2 className="modulo-menu">Gestión de usuarios</h2>
+                <img src={administracion} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Administración</h2>
                 </CardBody>
             </Card>
             </Col>    
@@ -23,8 +23,8 @@ export default class Menu extends Component {
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
             <Card onClick={() => this.props.history.push(this.props.match.path + 'presupuesto')} body outline color="success">
                 <CardBody>
-                <img src={budget} className="iconos-menu"/>    
-                <h2 className="modulo-menu">Gestión presupuestal</h2>
+                <img src={presupuesto} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Presupuesto</h2>
                 </CardBody>
             </Card>
             </Col>
@@ -33,8 +33,8 @@ export default class Menu extends Component {
             <Col className="opcion-menu-principal" xs={12} sm={12} md={4} lg={4}>
             <Card onClick={() => this.props.history.push(this.props.match.path + 'objetivos-especificos')} body outline color="success">
                 <CardBody>
-                <img src={solution} className="iconos-menu"/>    
-                <h2 className="modulo-menu">Gestión de planeación</h2>
+                <img src={planeacion} className="iconos-menu"/>    
+                <h2 className="modulo-menu">Planeación</h2>
                 </CardBody>
             </Card>
             </Col>
