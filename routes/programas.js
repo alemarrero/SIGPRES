99807969a5ae57948e7f512cc4beecd76fbdbed3
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-08 10:37:53 -0400

    versión inicial de los endpoints de gestión de programas

---
+++ routes/programas.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+var autorizarDirector = require('../controllers/autenticacion/autorizarDirector');
+var autorizarAdministrador = require('../controllers/autenticacion/autorizarAdministrador');
+var models = require('../models');
+
+/* */
+router.post('/crear_programa', autorizarDirector, function(req, res, next) {
+  models.programas.create({
+    nombre: req.body.nombre,
+    fecha_inicio: req.body.fecha_inicio,
+    fecha_finalizacion: req.body.fecha_finalizacion,
+    duracion: req.body.duracion,
+    direccion_id: 1,
+    descripcion: req.body.descripcion
+  })
+  .then(resultado => {
+    if(resultado){
+      res.status(200).json('ok');
+    }
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+router.post('/actualizar_programa', autorizarDirector, function(req, res, next){
+  models.programas.update({
+    nombre: req.body.nombre,
+    fecha_inicio: req.body.fecha_inicio,
+    fecha_finalizacion: req.body.fecha_finalizacion,
+    duracion: req.body.duracion,
+    direccion_id: 1,
+    descripcion: req.body.descripcion
+  },
+  {where: {id: req.body.id}}
+  )
+  .then(resultado => {
+    if(resultado[0]){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+router.post('/eliminar_programa', autorizarAdministrador, function(req, res){
+  models.programas.destroy({where: {id: req.body.id}})
+  .then(resultado => {
+    if(resultado){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+router.get('/obtener_programas', autorizarDirector, function(req, res){
+  models.programas.findAll()
+  .then( resultado => {
+    res.json(resultado).status(200);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+module.exports = router;
