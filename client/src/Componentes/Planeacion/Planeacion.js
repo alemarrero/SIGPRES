5d5aa290d310671f0bc19df5c8310f3de9be5808
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-08 11:44:24 -0400

    se crea la versión inicial del menu de planeación

---
+++ client/src/Componentes/Planeacion/Planeacion.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import OpcionMenu from '../Menu/OpcionMenu';
+import usuarios from '../../assets/img/usuarios.png';
+import { Row } from 'reactstrap';
+
+export default class Planeacion extends Component {
+  render() {
+    return (
+      <Row className="fila-opciones-menu-principal">
+        {/* Gestión de usuarios */}
+        <OpcionMenu ruta={'/programas/'} nombre="Gestión de Programas" icono={usuarios}/>
+      </Row>
+    )
+  }
+}
