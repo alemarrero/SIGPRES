a0dcaf21f87a626042bfa9d077429da8a68caa92
Alessandra Marrero <alemarrero16@gmail.com>
2018-10-08 12:47:21 -0400

    se crean las rutas para areas

---
+++ routes/areas.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+var autorizarAdministrador = require('../controllers/autenticacion/autorizarAdministrador');
+var models = require('../models');
+
+
+router.post('/crear_area', autorizarAdministrador, function(req, res, next) {
+  models.areas
+  .create({
+    nombre: req.body.nombre, 
+    descripcion: req.body.descripcion
+  })  
+  .then(response => {
+    if(response){
+      res.json('ok').status(200);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.json('err').status(500);
+  });
+});
+
+
+router.post('/actualizar_area', autorizarAdministrador, function(req, res){
+  models.areas.update({
+    nombre: req.body.nombre,
+    habilitado: req.body.habilitado,
+    descripcion: req.body.descripcion
+  },
+  {where: {id: req.body.id}})
+  .then(resultado => {
+    if(resultado[0]){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+router.post('/eliminar_area', autorizarAdministrador, function(req, res){
+  models.areas.destroy({where: {id: req.body.id}})
+  .then(resultado => {
+    if(resultado){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+
+router.post('/habilitar_area', autorizarAdministrador, function(req, res){
+  models.areas.update({
+    habilitado: true 
+  }, {where: {id: req.body.id}})
+  .then(x => {
+    if(x[0]){
+      res.json('ok').status(200);
+    }
+    else{
+      res.json('err').status(404);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+router.post('/deshabilitar_area', autorizarAdministrador, function(req, res){
+  models.areas.update({
+    habilitado: false 
+  }, {where: {id: req.body.id}})
+  .then(x => {
+    if(x[0]){
+      res.json('ok').status(200);
+    }
+    else{
+      res.json('err').status(404);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+router.get('/obtener_areas', autorizarAdministrador, function(req, res){
+  models.areas.findAll()
+  .then( resultado => {
+    res.json(resultado).status(200);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+module.exports = router;
