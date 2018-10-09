5c1b26fa3fb63818a14d19d14fc858fcce614cc9
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-09 15:51:50 -0400

    se arregla verificación de roles al autorizar director

--- controllers/autenticacion/autorizarDirector.js
+++ controllers/autenticacion/autorizarDirector.js
@@ -2,7 +2,7 @@ let autorizarDirector =  function(req, res, next){
     if(!req.session.autenticado){
         res.status(401).json('err');    
     }
-    else if(req.session.autenticado && (req.session.rol !== 'administrador' || req.session.rol !== 'director')){
+    else if(req.session.rol !== 'administrador' && req.session.rol !== 'director'){
         res.status(403).json('err');
     }
     else{
