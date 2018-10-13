f0ce36bb25baac4841b103adeac6b7943758c25f
Alessandra Marrero <alemarrero16@gmail.com>
2018-10-13 14:27:09 -0400

    se crea modelo y migraciones de las subespecificas

---
+++ models/subespecificas.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const subespecificas = sequelize.define('subespecificas', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    numero_subespecifica: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },    
+    especifica_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    denominacion: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },   
+    habilitada: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    timestamps: false,
+    freezeTableName: true
+  });
+  subespecificas.associate = function(models) {
+    subespecificas.belongsTo(models.especificas, {
+      foreignKey: 'especifica_id',
+    })  };
+  return subespecificas;
+};
