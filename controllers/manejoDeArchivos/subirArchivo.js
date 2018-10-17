6c8bc6cb479472d4060d2b0b9774288a4084f9a2
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-17 16:46:16 -0400

    controlador que se encarga de subir un archivo a cloudinary

---
+++ controllers/manejoDeArchivos/subirArchivo.js
@@ -0,0 +1,33 @@
+var cloudinary = require('cloudinary').v2;
+
+/**
+ * Controlador que se encarga de subir el archivo que acaba de subir el usuario a cloudinary y se encuentra
+ * almacenado en la carpeta /temp.
+ * 
+ * @param {String} preset Representa el preset a ser utilizado dentro de cloudinary; este valor puede ser:
+ * uno de los siguientes: 'planes_nacion', 'planes_cgr', 'planes_cmb' o 'planes_alcaldia'.
+ * 
+ * @return almacena en req.file_url la URL que devolvió cloudinary y en req.public_id el Public ID que 
+ * devolvio cloudinary, y llama a next() para ejecutar el siguiente middleware.
+ */
+let subirArchivo =  function(preset){
+  return function(req, res, next){
+    cloudinary.uploader.unsigned_upload(
+      req.file.path, 
+      preset, 
+      function(error, result) {
+        console.log(result, error);
+        if(error){
+          res.status(500).json('err');
+        }
+        else{
+          console.log(result);
+          req.file_url = result.url;
+          req.public_id = result.public_id;
+          next();
+        }
+      });
+  }
+};
+
+module.exports = subirArchivo;
