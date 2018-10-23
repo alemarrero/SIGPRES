5c1dcdbe4994e4f40ab89c9b5d9a837f3a4395ff
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-22 20:18:40 -0400

    se crea menu para acceder a la gestión de planes históricos

---
+++ client/src/Componentes/PlanesHistoricos/PlanesHistoricos.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import OpcionMenu from '../Menu/OpcionMenu';
+import nacion from '../../assets/img/venezuela.png';
+import cgr from '../../assets/img/cgr.png';
+import contraloria from '../../assets/img/contraloria.png';
+import alcaldia from '../../assets/img/alcaldia.png';
+import { Row } from 'reactstrap';
+
+export default class PlanesHistoricos extends Component {
+  render() {
+    return (
+      <Row className="fila-opciones-menu-planes-historicos">
+        {/* Gestión de Planes de la Nación */}
+        <OpcionMenu ruta={'/planes-nacion'} nombre="Planes Operativos de la Nación" icono={nacion}/>
+
+        {/* Gestión de Planes de la CGR */}
+        <OpcionMenu ruta={'/planes-cgr'} nombre="Planes Operativos de la CGR" icono={cgr}/>
+
+        {/* Gestión de Planes de la Alcaldía */}
+        <OpcionMenu ruta={'/planes-cgr'} nombre="Planes Operativos de la Alcaldía" icono={alcaldia}/>
+
+        {/* Gestión de Planes de la CMB */}
+        <OpcionMenu ruta={'/planes-cmb'} nombre="Planes Operativos de la CMB" icono={contraloria}/>
+      </Row>
+    )
+  }
+}
