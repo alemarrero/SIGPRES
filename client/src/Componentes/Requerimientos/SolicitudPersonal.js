a3656c81b6c092b642ff5f6ea2f77fbe83dd0d0e
Alessandra Marrero <alemarrero16@gmail.com>
2018-10-24 00:01:11 -0400

    Se agrega modulo de gestion de cargos al modulo de solicitud de personal

--- client/src/Componentes/Requerimientos/SolicitudPersonal.js
+++ client/src/Componentes/Requerimientos/SolicitudPersonal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Row, Col, Card, CardBody} from 'reactstrap';
 import './Requerimientos.css';
 import personal from '../../assets/img/personal.png';
+import cargo from '../../assets/img/cargo.png';
 import requerimientosViejos from '../../assets/img/requerimientos-viejos.png';
 import OpcionMenu from '../Menu/OpcionMenu';
 
@@ -14,6 +15,9 @@ export default class Menu extends Component {
         
         {/* Solicitud de requerimientosViejos */}
         <OpcionMenu ruta={'solicitudes-anteriores/'} nombre="Solicitud de Personal de años anteriores" icono={requerimientosViejos}/>
+        
+        {/* Gestion de cargos */}   
+        <OpcionMenu ruta={'/cargos/'} nombre="Gestión de Cargos" icono={cargo}/>        
       </Row>
     )
   }
