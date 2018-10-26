451f07a480e262538a685ac3daa0ba3e175f62e6
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-26 16:45:18 -0400

    se crean esqueletos iniciales de los componentes de gestión de atención al ciudadano

---
+++ client/src/Componentes/AtencionCiudadano/PresupuestoParticipativo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import usuarios from '../../assets/img/usuarios.png';
+import { Row } from 'reactstrap';
+
+export default class PresupuestoParticipativo extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
