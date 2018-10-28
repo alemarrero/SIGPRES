ada31e3c591dd698bc815a1ddf3b856c32851552
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-28 12:46:58 -0400

    esqueleto de la gestión administrativa de presupuesto participativo

---
+++ client/src/Componentes/AtencionCiudadano/GestionPresupuestoParticipativo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import usuarios from '../../assets/img/usuarios.png';
+import { Row } from 'reactstrap';
+
+export default class PresupuestoParticipativo extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
