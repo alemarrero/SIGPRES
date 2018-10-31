e47050700dea20311a5ca8e22396b8ab9829c33c
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-31 19:52:24 -0400

    hotfix

--- migrations/20181031194600-fix-fk-requerimientos-personal-a-solicitud-personal.js
+++ migrations/20181031194600-fix-fk-requerimientos-personal-a-solicitud-personal.js
@@ -1,20 +1,9 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('requerimientos_personal', 'solicitud_personal_id', {
-      type: Sequelize.INTEGER,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'solicitud_personal',
-          key: 'id'
-        },
-    }, {
-      timestamps: false
-    });
+    return true;
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('requerimientos_personal', 'solicitud_personal_id', {
-      timestamps: false
-    });
+    return true;
   }
 };
