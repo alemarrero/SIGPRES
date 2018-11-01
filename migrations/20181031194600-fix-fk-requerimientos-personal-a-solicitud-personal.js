dbe79c9b2ab6cb17189977e293202f9e4e84f371
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-01 00:13:21 -0400

    se terminan de arreglar las migraciones

--- migrations/20181031194600-fix-fk-requerimientos-personal-a-solicitud-personal.js
+++ migrations/20181031194600-fix-fk-requerimientos-personal-a-solicitud-personal.js
@@ -1,9 +1,19 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return true;
+    return queryInterface.addColumn('requerimientos_personal', 'solicitud_personal_id', {
+      type: Sequelize.INTEGER,
+        references: {
+          model: 'solicitud_personal',
+          key: 'id'
+        },
+    }, {
+      timestamps: false
+    });
   },
   down: (queryInterface, Sequelize) => {
-    return true;
+    return queryInterface.removeColumn('requerimientos_personal', 'solicitud_personal_id', {
+      timestamps: false
+    });
   }
 };
