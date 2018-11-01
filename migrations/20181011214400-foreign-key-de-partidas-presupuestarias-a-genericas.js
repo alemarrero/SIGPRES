228761c0f89684b0736bdde43df5b5e2ad81daed
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-31 23:38:00 -0400

    se arregla validation error de migration es heroku

--- migrations/20181011214400-foreign-key-de-partidas-presupuestarias-a-genericas.js
+++ migrations/20181011214400-foreign-key-de-partidas-presupuestarias-a-genericas.js
@@ -1,22 +1,10 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.changeColumn('genericas', 'partida_presupuestaria_id', {
-      type: Sequelize.INTEGER,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'partidas_presupuestarias',
-          key: 'id'
-        },
-    }, {
-      timestamps: false
-    });
+    return true;
+      
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.changeColumn('genericas', 'partida_presupuestaria_id', {
-      allowNull: false,
-      unique: true,           
-      type: Sequelize.INTEGER      
-    });
+    return true;
   }
 };
