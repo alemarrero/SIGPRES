091dbca072b0aa477e3e0886e5dd246ac8eafb88
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-10-31 23:42:28 -0400

    Revert "se arregla validation error de migration es heroku"
    This reverts commit 228761c0f89684b0736bdde43df5b5e2ad81daed.

--- migrations/20181011214400-foreign-key-de-partidas-presupuestarias-a-genericas.js
+++ migrations/20181011214400-foreign-key-de-partidas-presupuestarias-a-genericas.js
@@ -1,10 +1,22 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return true;
-      
+    return queryInterface.changeColumn('genericas', 'partida_presupuestaria_id', {
+      type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'partidas_presupuestarias',
+          key: 'id'
+        },
+    }, {
+      timestamps: false
+    });
   },
   down: (queryInterface, Sequelize) => {
-    return true;
+    return queryInterface.changeColumn('genericas', 'partida_presupuestaria_id', {
+      allowNull: false,
+      unique: true,           
+      type: Sequelize.INTEGER      
+    });
   }
 };
