f2295e093b7947267e0bde0c979052f5091695d3
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-01 17:54:16 -0400

    se agregan seeders iniciales

--- seeders/20181009200144-administrador-de-prueba.js
+++ seeders/20181009200144-administrador-de-prueba.js
@@ -2,20 +2,55 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('usuarios', [{
-      nombre: "Administrador",
-      apellido: "de Prueba",
-      cedula: "V-1234567",
-      fecha_ingreso: "2000-01-02",
-      fecha_nacimiento: "2000-01-01",
-      departamento: "IT",
-      cargo: "Director de IT",
-      rol: "administrador",
-      habilitado: true,
-      correo: 'admin@admin.com',
-      usuario: 'admin',
-      password: "$2b$10$5prGTct1Pd60NJ1QJtyRbuPiOr4ilhoqZj65//7T/ta/4PRQH7FOm"
-    }], {});
+    try {
+      return queryInterface.bulkInsert('usuarios', [
+        {
+          nombre: "Administrador",
+          apellido: "de Prueba",
+          cedula: "V-1234567",
+          fecha_ingreso: "2000-01-02",
+          fecha_nacimiento: "2000-01-01",
+          area_id: process.env.AREA_ID,
+          cargo: "Director de Tecnología de Información y Comunicación",
+          rol: "administrador",
+          habilitado: true,
+          correo: 'admin@admin.com',
+          usuario: 'admin',
+          password: "$2b$10$5prGTct1Pd60NJ1QJtyRbuPiOr4ilhoqZj65//7T/ta/4PRQH7FOm"
+        },
+        {
+          nombre: "Director",
+          apellido: "de Prueba",
+          cedula: "V-1234567",
+          fecha_ingreso: "2000-01-02",
+          fecha_nacimiento: "2000-01-01",
+          area_id: process.env.AREA_ID,
+          cargo: "Director de Tecnología de Información y Comunicación",
+          rol: "director",
+          habilitado: true,
+          correo: 'director@director.com',
+          usuario: 'director',
+          password: "$2b$10$5prGTct1Pd60NJ1QJtyRbuPiOr4ilhoqZj65//7T/ta/4PRQH7FOm"
+        },
+        {
+          nombre: "Usuario regular",
+          apellido: "de Prueba",
+          cedula: "V-1234567",
+          fecha_ingreso: "2000-01-02",
+          fecha_nacimiento: "2000-01-01",
+          area_id: process.env.AREA_ID,
+          cargo: "Secretario",
+          rol: "regular",
+          habilitado: true,
+          correo: 'usuario@usuario.com',
+          usuario: 'usuario',
+          password: "$2b$10$5prGTct1Pd60NJ1QJtyRbuPiOr4ilhoqZj65//7T/ta/4PRQH7FOm"
+        },
+      ], {});
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 
   down: (queryInterface, Sequelize) => {
