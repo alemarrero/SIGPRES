5f6c55531720a639bfad247c13d40f969c432cf2
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-05 13:55:52 -0400

    se obtienen las propuestas de POAs

--- client/src/Componentes/ObjetivosEspecificos/RevisionPOA.js
+++ client/src/Componentes/ObjetivosEspecificos/RevisionPOA.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import {Row, Col, Button} from 'reactstrap';
 import './DetallePOA.css';
 
-export default class DetallePOA extends Component {
+export default class RevisionPOA extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      propuestas: [],
+      modal_operacion_fallida: false,
+      mensaje: undefined,
+    };
+    this.obtenerPropuestas = this.obtenerPropuestas.bind(this);
+  }
+
+  async componentDidMount(){
+    document.title = "SICMB - Revisión de POA";
+    this.obtenerPropuestas();
+  }
+
+  async obtenerPropuestas(){
+    const request = await fetch("/api/propuestas_plan_operativo_anual/obtener_propuestas", {credentials: "include"});
+    const response = await request.json();
+
+    if(response !== "err"){
+      this.setState({propuestas: response});
+    }
+    else{
+      this.setState({modal_operacion_fallida: true, mensaje: "Error al obtener las propuestas de las direcciones"});
+    }
+  }
+
   render() {
     return (
       <Row>
