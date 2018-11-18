cecc4e94c8131a8b1a8c4716a59d036c7e400aac
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-18 12:27:24 -0400

    se crea endpoint para la carga de datos a través de hojas de cálculo

--- routes/partidas_presupuestarias.js
+++ routes/partidas_presupuestarias.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var autorizarAdministrador = require('../controllers/autenticacion/autorizarAdministrador');
 var models = require('../models');
 var recibirArchivo = require('../controllers/manejoDeArchivos/recibirArchivos');
-
+var XLSX = require('xlsx');
 
 router.post('/crear_partida_presupuestaria', autorizarAdministrador, function(req, res, next) {
   models.partidas_presupuestarias
@@ -226,7 +226,255 @@ router.post('/obtener_partida_desde_subespecifica', function(req,res){
 
 });
 
-router.post('/cargar_cuentas', recibirArchivo, function(req, res){
+router.post('/cargar_partidas', recibirArchivo, async function(req, res){
+  // Contiene el archivo XLS subido por el usuario
+  const workbook = XLSX.readFile(req.file.path);
+  const cuentas = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]]);
+  let partidas = cuentas.filter(cuenta => cuenta.tipo === "partida presupuestaria");
+
+  // Se introducen las partidas en la base de datos
+  cargarPartidasPresupuestarias(partidas, res);
+
   res.status(200).json("ok");
 });
+
+router.post('/cargar_genericas', recibirArchivo, async function(req, res){
+  // Contiene el archivo XLS subido por el usuario
+  const workbook = XLSX.readFile(req.file.path);
+  const cuentas = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]]);
+  let genericas = cuentas.filter(cuenta => cuenta.tipo === "generica");
+
+  // // Se introducen las genéricas
+  cargarPartidasGenericas(genericas, res);
+
+  res.status(200).json("ok");
+});
+
+router.post('/cargar_especificas', recibirArchivo, async function(req, res){
+  // Contiene el archivo XLS subido por el usuario
+  const workbook = XLSX.readFile(req.file.path);
+  const cuentas = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]]);
+  let especificas = cuentas.filter(cuenta => cuenta.tipo === "especifica");
+
+  // // Se introducen las específicas
+  cargarPartidasEspecificas(especificas);
+
+  res.status(200).json("ok");
+});
+
+router.post('/cargar_subespecificas', recibirArchivo, async function(req, res){
+  // Contiene el archivo XLS subido por el usuario
+  const workbook = XLSX.readFile(req.file.path);
+  const cuentas = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]]);
+  let subespecificas = cuentas.filter(cuenta => cuenta.tipo === "subespecifica");
+
+  // Se introducen las subespecíficas
+  cargarPartidasSubespecificas(subespecificas);
+
+  res.status(200).json("ok");
+});
+
+
 module.exports = router;
+
+function cargarPartidasSubespecificas(subespecificas) {
+  Promise.all(subespecificas.map(subespecifica => {
+    let numero_partida = subespecifica.numero.split(".");
+    let numero_especifica = `${numero_partida[3]}`;
+    let numero_subespecifica = `${numero_partida[4]}`;
+    let id_partida_especifica;
+    // Primero se busca en la base de datos el id de la partida específica a la cual
+    // está asociada la subespecífica
+    models.especificas.findOne({ where: { numero_especifica: numero_especifica } })
+      .then(partida_especifica => {
+        id_partida_especifica = partida_especifica.dataValues.id;
+        // Se busca en la base de datos si la subespecífica ya existe
+        models.subespecificas.findOne({ where: { numero_subespecifica: numero_subespecifica, especifica_id: id_partida_especifica } })
+          .then(resultado => {
+            // Si ya existe, entonces se actualiza con la nueva información
+            if (resultado) {
+              models.subespecificas.update({ denominacion: subespecifica.denominacion }, { where: { id: resultado.dataValues.id } })
+                .then(subespecifica_actualizada => {
+                  if (subespecifica_actualizada[0]) {
+                    console.log(`Partida subespecífica ${numero_subespecifica} actualizada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+            else {
+              // De lo contrario, entonces se crea la subespecífica
+              models.subespecificas.create({
+                numero_subespecifica: numero_subespecifica,
+                denominacion: subespecifica.denominacion,
+                especifica_id: id_partida_especifica
+              })
+                .then(subespecifica_creada => {
+                  if (subespecifica_creada) {
+                    console.log(`Partida subespecífica ${numero_subespecifica} creada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }));
+}
+
+function cargarPartidasEspecificas(especificas) {
+  Promise.all(especificas.map(especifica => {
+    let numero_partida = especifica.numero.split(".");
+    let numero_generica = `${numero_partida[2]}`;
+    let numero_especifica = `${numero_partida[3]}`;
+    let id_partida_generica;
+    // Primero se busca en la base de datos el id de la partida genérica a la cual
+    // está asociada la específica
+    models.genericas.findOne({ where: { numero_generica: numero_generica } })
+      .then(partida_generica => {
+        id_partida_generica = partida_generica.dataValues.id;
+        // Se busca en la base de datos si la específica ya existe
+        models.especificas.findOne({ where: { numero_especifica: numero_especifica, generica_id: id_partida_generica } })
+          .then(resultado => {
+            // Si ya existe, entonces se actualiza con la nueva información
+            if (resultado) {
+              models.especificas.update({ denominacion: especifica.denominacion }, { where: { id: resultado.dataValues.id } })
+                .then(especifica_actualizada => {
+                  if (especifica_actualizada[0]) {
+                    console.log(`Partida específica ${numero_especifica} actualizada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+            else {
+              // De lo contrario, entonces se crea la específica
+              models.especificas.create({
+                numero_especifica: numero_especifica,
+                denominacion: especifica.denominacion,
+                generica_id: id_partida_generica
+              })
+                .then(especifica_creada => {
+                  if (especifica_creada) {
+                    console.log(`Partida específica ${numero_especifica} creada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }));
+}
+
+function cargarPartidasGenericas(genericas, res) {
+  Promise.all(genericas.map(generica => {
+    let numero_partida = generica.numero.split(".");
+    let numero_partida_presupuestaria = `${numero_partida[0]}${numero_partida[1]}`;
+    let numero_generica = `${numero_partida[2]}`;
+    let id_partida_presupuestaria;
+    // Primero se busca en la base de datos el id de la partida presupuestaria a la cual
+    // está asociada la genérica
+    models.partidas_presupuestarias.findOne({ where: { numero_partida: numero_partida_presupuestaria } })
+      .then(partida_presupuestaria => {
+        id_partida_presupuestaria = partida_presupuestaria.dataValues.id;
+        // Se busca en la base de datos si la genérica ya existe
+        models.genericas.findOne({ where: { numero_generica: numero_generica, partida_presupuestaria_id: id_partida_presupuestaria } })
+          .then(resultado => {
+            // Si ya existe, entonces se actualiza con la nueva información
+            if (resultado) {
+              models.genericas.update({ denominacion: generica.denominacion }, { where: { id: resultado.dataValues.id } })
+                .then(generica_actualizada => {
+                  if (generica_actualizada[0]) {
+                    console.log(`Partida genérica ${numero_generica} actualizada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+            else {
+              // De lo contrario, entonces se crea la genérica
+              models.genericas.create({
+                numero_generica: numero_generica,
+                denominacion: generica.denominacion,
+                partida_presupuestaria_id: id_partida_presupuestaria
+              })
+                .then(generica_creada => {
+                  if (generica_creada) {
+                    console.log(`Partida genérica ${numero_generica} creada correctamente.`);
+                  }
+                })
+                .catch(err => {
+                  console.log(err);
+                  // res.status(500).json("err");
+                });
+            }
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }));
+}
+
+function cargarPartidasPresupuestarias(partidas, res) {
+  Promise.all(partidas.map(partida => {
+    let numero_partida = partida.numero.split(".");
+    // Se busca si la partida existe previamente en la base de datos
+    models.partidas_presupuestarias.findOne({ where: { numero_partida: `${numero_partida[0]}${numero_partida[1]}` } })
+      .then(resultado => {
+        // Si existe, se actualiza con la nueva denominación
+        if (resultado) {
+          models.partidas_presupuestarias.update({ denominacion: partida.denominacion }, { where: { id: resultado.dataValues.id } })
+            .then(partida_actualizada => {
+              if (partida_actualizada[0]) {
+                console.log(`Partida ${numero_partida[0]}${numero_partida[1]} actualizada correctamente.`);
+              }
+            });
+        }
+        else {
+          // Si no existe, entonces se crea
+          models.partidas_presupuestarias.create({
+            numero_partida: `${numero_partida[0]}${numero_partida[1]}`,
+            denominacion: partida.denominacion
+          })
+            .then(res2 => {
+              if (res2) {
+                console.log(`Partida ${numero_partida[0]}${numero_partida[1]} creada correctamente.`);
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              res.status(500).json("err");
+            });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json("err");
+      });
+  }));
+}
+
