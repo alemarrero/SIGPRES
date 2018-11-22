d119f2d1f23d81a6fe1a423da3fdeaff4527a7ba
Alessandra Marrero <alemarrero16@gmail.com>
2018-11-21 23:28:23 -0400

    Se modifica modelo de solicitudes de requerimientos para incluir delete on cascade

--- models/solicitudes_de_requerimientos.js
+++ models/solicitudes_de_requerimientos.js
@@ -32,6 +32,8 @@ module.exports = (sequelize, DataTypes) => {
     solicitudes_de_requerimientos.hasMany(models.entradas_solicitud_de_requerimientos, {
       foreignKey: 'solicitud_id',
       as: 'requerimientos',
+      onDelete: 'cascade',
+      hooks: true
     });
 
     solicitudes_de_requerimientos.belongsTo(models.areas, {
