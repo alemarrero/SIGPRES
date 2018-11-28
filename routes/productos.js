21edf3587b26653013ef02bc7e5c3446e413835d
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-28 17:06:32 -0400

    se crea endpoint para realizar carga masiva de productos en la base de datos

--- routes/productos.js
+++ routes/productos.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var autorizarAdministrador = require('../controllers/autenticacion/autorizarAdministrador');
 var models = require('../models');
-
+var recibirArchivo = require('../controllers/manejoDeArchivos/recibirArchivos');
+var XLSX = require('xlsx');
 
 router.post('/crear_producto', autorizarAdministrador, function(req, res, next) {
   console.log(req.body.subespecifica_id);
@@ -300,5 +301,185 @@ router.get('/obtener_consolidado_presupuesto', function(req, res){
     res.status(500).json(err);
   })
 });
+
+router.post('/cargar_productos', 
+    recibirArchivo, 
+    
+    function(req, res, next){
+      // Contiene el archivo XLS subido por el usuario
+      const workbook = XLSX.readFile(req.file.path);
+      // console.log(workbook.Sheets);
+      const productos = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+
+      // // Se introducen las genéricas
+      cargarProductos(productos, next);
+    },
+
+    function(req, res){
+      res.status(200).json("ok");
+    }
+);
+
+function cargarProductos(productos, next){
+  Promise.all(productos.map(
+    producto => {
+      let especifica_id;
+      let subespecifica_id;
+      let unidad_de_medida_id;
+      let iva = producto.porcentaje_iva < 1 ? producto.porcentaje_iva * 100 : producto.porcentaje_iva ;
+  
+      // Se busca el id de la unidad de medida asociada al producto. 
+      // Si la unidad de medida no se encuentra en la base de datos
+      // se crea una nueva unidad de medida
+      models.unidades_de_medida.findOne({where: {nombre: producto.unidad_de_medida}})
+      .then(unidad_de_medida => {
+        if(unidad_de_medida){
+          unidad_de_medida_id = unidad_de_medida.dataValues.id;
+        }
+        else{
+          models.unidades_de_medida.create({nombre: producto.unidad_de_medida, tipo: "productos"})
+          .then(unidad_de_medida => {
+            unidad_de_medida_id = unidad_de_medida.dataValues.id;
+          })
+          .catch(err => {
+            console.err(err);
+          });
+        }
+  
+        // Se verifica si el producto pertenece a una partida subespecífica (subespecifica !== 00)
+        // o si pertenece a una partida específica (especifica === 00)
+        if(producto.subespecifica === "00"){
+          models.especificas.findOne({where: {numero_especifica: producto.especifica}})
+          .then(especifica => {
+            especifica_id = especifica.dataValues.id;
+            
+            // Se verifica si el producto ya se encuentra en la base de datos;
+            // en caso de encontrarse, se actualiza su información,
+            // de lo contrario, se crea un producto nuevo
+            models.productos.findOne({where: {codigo: producto.codigo}})
+            .then(prod => {
+              if(prod){
+                models.productos.update({
+                  nombre: `${producto.nombre}`,
+                  precio: parseFloat(producto.precio, 10),
+                  especifica_id: especifica_id,
+                  unidad_de_medida_id: unidad_de_medida_id,
+                  iva: parseFloat(iva, 10),
+                  monto_iva: parseFloat(producto.monto_iva, 10),
+                  total: parseFloat(producto.precio_total, 10)
+                }, 
+                {where: {id: prod.dataValues.codigo}})
+                .then(producto_actualizado => {
+                  if(producto_actualizado[0]){
+                    console.log(`Producto ${producto.nombre} actualizado correctamente`);
+                  }
+                })
+                .catch(err => {
+                  console.log(`Error al actualizar el producto ${producto_actualizado.dataValues.nombre}`);
+                  console.log(err);
+                });
+              }
+              else{
+                models.productos.create({
+                  codigo: producto.codigo,
+                  nombre: `${producto.nombre}`,
+                  precio: parseFloat(producto.precio, 10),
+                  especifica_id: especifica_id,
+                  unidad_de_medida_id: unidad_de_medida_id,
+                  iva: parseFloat(iva, 10),
+                  monto_iva: parseFloat(producto.monto_iva, 10),
+                  total: parseFloat(producto.precio_total, 10)
+                })
+                .then(producto_creado => {
+                  if(producto_creado){
+                    console.log(`Producto ${producto.nombre} creado`);
+                  }
+                })
+                .catch(err => {
+                  console.log(`Error creando el producto ${producto.nombre}`);
+                  console.log(err);
+                });
+              }
+            })
+            
+          })
+          .catch(err => {
+            console.log('Error al procesar el producto ');
+            console.log(producto.nombre);
+          });
+        }
+        else{
+          models.subespecificas.findOne({where: {numero_subespecifica: producto.subespecifica}})
+          .then(subespecifica => {
+            subespecifica_id = subespecifica.dataValues.id;
+  
+            // Se verifica si el producto ya se encuentra en la base de datos;
+            // en caso de encontrarse, se actualiza su información,
+            // de lo contrario, se crea un producto nuevo
+            models.productos.findOne({where: {codigo: producto.codigo}})
+            .then(prod => {
+              if(prod){
+                models.productos.update({
+                  nombre: `${producto.nombre}`,
+                  nombre: `${producto.nombre}`,
+                  precio: parseFloat(producto.precio, 10),
+                  subespecifica_id: subespecifica_id,
+                  unidad_de_medida_id: unidad_de_medida_id,
+                  iva: parseFloat(iva, 10),
+                  monto_iva: parseFloat(producto.monto_iva, 10),
+                  total: parseFloat(producto.precio_total, 10)
+                }, 
+                {where: {id: prod.dataValues.codigo}})
+                .then(producto_actualizado => {
+                  console.log(producto_actualizado);
+                  if(producto_actualizado[0]){
+                    // console.log(`Producto ${producto.nombre} actualizado correctamente`);
+                  }
+                })
+                .catch(err => {
+                  console.log(`Error al actualizar el producto ${producto_actualizado.dataValues.nombre}`);
+                  console.log(err);
+                });
+              }
+              else{
+                models.productos.create({
+                  codigo: producto.codigo,
+                  nombre: `${producto.nombre}`,
+                  precio: parseFloat(producto.precio, 10),
+                  subespecifica_id: subespecifica_id,
+                  unidad_de_medida_id: unidad_de_medida_id,
+                  iva: parseFloat(iva, 10),
+                  monto_iva: parseFloat(producto.monto_iva, 10),
+                  total: parseFloat(producto.precio_total, 10)
+                })
+                .then(producto_creado => {
+                  if(producto_creado){
+                    console.log(`Producto ${producto.nombre} creado`);
+                  }
+                })
+                .catch(err => {
+                  console.log(`Error creando el producto ${producto.nombre}`);
+                  // console.log(err);
+                });
+              }
+            })
+  
+          })
+          .catch(err => {
+            console.error(err);
+            // res.status(500).json("err")
+          });
+        }
+      })
+      .catch(err => {
+        console.log('Error al procesar el producto (linea 474)');
+        console.log(producto);
+      });
+    }
+  ))
+  .then(() => {
+    next();
+  });
+}
 
 module.exports = router;
