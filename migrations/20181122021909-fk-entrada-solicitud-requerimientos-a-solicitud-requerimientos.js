6ac22f9851de3eba0da9b8d18985f7b59514b4f6
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-28 19:48:05 -0400

    se arregla migración para poder hacer build en heroku

--- migrations/20181122021909-fk-entrada-solicitud-requerimientos-a-solicitud-requerimientos.js
+++ migrations/20181122021909-fk-entrada-solicitud-requerimientos-a-solicitud-requerimientos.js
@@ -1,17 +1,19 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('entradas_solicitud_de_requerimientos', 'solicitud_id', {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      onDelete: 'CASCADE',      
-      references: {
-        model: "solicitudes_de_requerimientos",
-        key: "id"
-      },
-     }, {
-        timestamps: false
-      });        
+    // return queryInterface.addColumn('entradas_solicitud_de_requerimientos', 'solicitud_id', {
+    //   type: Sequelize.INTEGER,
+    //   allowNull: false,
+    //   onDelete: 'CASCADE',      
+    //   references: {
+    //     model: "solicitudes_de_requerimientos",
+    //     key: "id"
+    //   },
+    //  }, {
+    //     timestamps: false
+    //   });    
+    
+    return true;
   }, 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('entradas_solicitud_de_requerimientos', 'solicitud_id', {
