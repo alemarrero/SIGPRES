5f2d6cc9cd2598dd821a1f121c73aab61c7af5a6
Julio Fuenmayor <jfuenmayor96@gmail.com>
2018-11-28 21:02:55 -0400

    hotfix de migración

---
+++ migrations/20181122021909-fix-fk-entrada-solicitud-requerimientos-a-solicitud-requerimientos.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn('entradas_solicitud_de_requerimientos', 'solicitud_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',      
+      references: {
+        model: "solicitudes_de_requerimientos",
+        key: "id"
+      },
+     }, {
+        timestamps: false
+      });    
+  }, 
+  down: (queryInterface, Sequelize) => {
+    // return queryInterface.removeColumn('entradas_solicitud_de_requerimientos', 'solicitud_id', {
+    //   timestamps: false
+    // })}
+    return true;
+  }
+
+};
