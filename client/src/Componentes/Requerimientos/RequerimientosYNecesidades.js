32c96a19d2e9e75db430f66b9ad4f58d089d0717
Alessandra Marrero <alemarrero16@gmail.com>
2018-12-05 12:23:39 -0400

    Se cambia nombre de opcion para no pensar que se entro a un ciclo

--- client/src/Componentes/Requerimientos/RequerimientosYNecesidades.js
+++ client/src/Componentes/Requerimientos/RequerimientosYNecesidades.js
@@ -12,7 +12,7 @@ export default class Menu extends Component {
     return (
       <Row className="fila-opciones-menu-principal">
         {/* Requerimientos y Necesidades */}   
-        <OpcionMenu ruta={'/solicitud-de-requerimientos/'} nombre="Requerimientos y Necesidades" icono={requerimientos}/>
+        <OpcionMenu ruta={'/solicitud-de-requerimientos/'} nombre="Solicitud de Requerimientos y Necesidades" icono={requerimientos}/>
         
         {/* Solicitud de requerimientosViejos */}
         <OpcionMenu ruta={'requerimientos-anteriores/'} nombre="Históricos" icono={requerimientosViejos}/>
