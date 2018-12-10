fc3dcfe7018ab7698ee32b91237d0a4fa0a81b4c
Alessandra Marrero <alemarrero16@gmail.com>
2018-12-10 17:30:44 -0400

    Se crean endpoints para vinculacion de acciones y productos

---
+++ routes/vinculacion_acciones_productos.js
@@ -0,0 +1,80 @@
+
+var express = require('express');
+var router = express.Router();
+var autorizarAdministrador = require('../controllers/autenticacion/autorizarAdministrador');
+var models = require('../models');
+
+router.post('/crear_vinculacion_accion_producto', autorizarAdministrador, function(req, res, next) {
+  models.vinculacion_acciones_productos.create({
+    accion_id: req.body.accion_id,
+    producto_id: req.body.producto_id 
+  })  
+  .then(response => {
+    if(response){
+      res.json('ok').status(200);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.json('err').status(500);
+  });
+});
+
+
+router.post('/actualizar_vinculacion_accion_producto', autorizarAdministrador, function(req, res){
+  models.vinculacion_acciones_productos.update({
+    accion_id: req.body.accion_id,
+    producto_id: req.body.producto_id  
+  },
+  {where: {id: req.body.id}})
+  .then(resultado => {
+    if(resultado[0]){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+router.post('/eliminar_vinculacion_accion_producto', autorizarAdministrador, function(req, res){
+  models.vinculacion_acciones_productos.destroy({where: {id: req.body.id}})
+  .then(resultado => {
+    if(resultado){
+      res.status(200).json('ok');
+    }
+    else{
+      res.status(404).json('err');
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  });
+});
+
+router.get('/obtener_vinculacion_acciones_productos', function(req, res){
+  models.vinculacion_acciones_productos.findAll({
+      include:[
+          {
+          model: models.productos,
+          as: 'productos',
+          }
+      ]
+  })
+  .then( resultado => {
+    res.json(resultado).status(200);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json('err');
+  })
+});
+
+
+
+module.exports = router;
