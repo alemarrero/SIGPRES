e2896a6f5d80439e0100593d819be904ed9713f4
Alessandra Marrero <alemarrero16@gmail.com>
2018-12-20 20:44:23 -0400

    Se agregan privilegios a la vista de requerimientos y necesidades

--- client/src/Componentes/Requerimientos/RequerimientosYNecesidades.js
+++ client/src/Componentes/Requerimientos/RequerimientosYNecesidades.js
@@ -6,8 +6,11 @@ import requerimientosViejos from '../../assets/img/requerimientos-viejos.png';
 import OpcionMenu from '../Menu/OpcionMenu';
 import consultar from '../../assets/img/consultar.png';
 import consolidado from '../../assets/img/consolidado.png';
+import withContext from './../../Contenedor/withContext';
+import autorizarDirectorPP from '../../Utilidades/autorizarDirectorPP.js';
 
-export default class Menu extends Component {
+
+export class Menu extends Component {
   render() {
     return (
 
@@ -32,14 +35,18 @@ export default class Menu extends Component {
           <OpcionMenu ruta={'requerimientos-anteriores/'} nombre="Históricos" icono={requerimientosViejos}/>
           
           {/* Consulta de solicitudes de requerimientos y necesidades */}   
+          {autorizarDirectorPP(this.props.usuario.rol) &&          
           <OpcionMenu ruta={'/consultar-solicitudes-de-requerimientos/'} nombre="Consultar Solicitudes de Requerimientos y Necesidades" icono={consultar}/> 
-
-          {/* Consulta consolidado de solicitudes de personal */}   
-          <OpcionMenu ruta={'/consolidado-solicitudes-requerimientos/'} nombre="Consultar Consolidado de Requerimientos y Necesidades" icono={consolidado}/>                 
+          }
         
+          {/* Consulta consolidado de solicitudes de personal */}   
+          {autorizarDirectorPP(this.props.usuario.rol) &&        
+          <OpcionMenu ruta={'/consolidado-solicitudes-requerimientos/'} nombre="Consultar Consolidado de Requerimientos y Necesidades" icono={consolidado}/>                 
+          }
         </Row>
       </React.Fragment>
 
     )
   }
 }
+export default withContext(Menu);
